6c7b6abbc11318daf92ade10eda81f087aac3b2e
shintiaadwim <shintiaar05@gmail.com>
2026-02-11 17:40:41 +0700

    installation & setup

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import Table from './components/Table';
+
+function App() {
+  return (
+    <div>
+      <Table />
+      {/* ...existing code... */}
+    </div>
+  );
+}
+
+export default App
